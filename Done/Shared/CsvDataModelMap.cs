843011846c647bca733329fc0bb98319ce19beea
TaNbir <ktahmmed@gmail.com>
2023-05-23 12:59:36 +0600

    Add project files.

---
+++ Done/Shared/CsvDataModelMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace Done.Shared
+{
+    public class CsvDataModelMap : ClassMap<CsvDataModel>
+    {
+        public CsvDataModelMap()
+        {
+            Map(m => m.Uid).Index(0);
+            Map(m => m.FirstName).Index(1);
+            Map(m => m.LastName).Index(2);
+            Map(m => m.Gender).Index(3);
+            Map(m => m.Birthday).Index(4);
+            Map(m => m.City).Index(5);
+            Map(m => m.State).Index(6);
+            Map(m => m.Zip).Index(7);
+            Map(m => m.SubscriptionDate).Index(8);
+        }
+    }
+}
